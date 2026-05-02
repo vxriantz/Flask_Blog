38aa021e85c216ea84af7f779a1bd3a6084daf42
vxriantz <20067@my.sanctamaria.school.nz>
2026-05-02 16:13:59 +1200

    video 7 - setting up the database (created models.py and integrated __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,20 @@
 
 # import external libraies
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 # create app function
 # returns app
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = "j@lZ?%NLgB$OAugIF*hbNlhjf1]b.ilA"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # import views from views.py
     from .views import views
@@ -16,5 +25,9 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import user
+
+    with app.app_context():
+        db.create_all()
 
     return app
---
+++ website/models.py
@@ -0,0 +1,13 @@
+from . import db
+from sqlalchemy.sql import func
+from flask_login import UserMixin
+
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.Integer(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
