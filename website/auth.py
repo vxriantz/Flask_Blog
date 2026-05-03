411851ef82584305a9a7d63a2bdb7d19e9c6de52
vxriantz <20067@my.sanctamaria.school.nz>
2026-05-03 13:07:12 +1200

    video 8 - setting up to create the signup route

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,12 @@
 
 # import external libraries
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+# import database
+from . import db
+# import from .models user
+from .models import User
 
 # set auth blueprint
 auth = Blueprint("auth", __name__)
@@ -10,7 +16,10 @@ auth = Blueprint("auth", __name__)
 @auth.route("/sign-up", methods=['GET', 'POST'])
 # sign up function
 # returns sign up page
+
 def sign_up():
+    # signup function
+    # returns signup_page
     return render_template("sign_up.html")
 
 # login route
--- website/models.py
+++ website/models.py
@@ -9,5 +9,5 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
-    password = db.Column(db.Integer(150))
+    password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
