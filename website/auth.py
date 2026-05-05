1b8012267353350500adab3942bb404fb4e8f3f3
vxriantz <20067@my.sanctamaria.school.nz>
2026-05-05 12:23:31 +1200

    video 10 - displaying flash messages (fixed the sign up route and page, database works and saves the inputted information, shows the correct flash messages and does not redirect or save when there is an error

--- website/auth.py
+++ website/auth.py
@@ -25,28 +25,28 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-    email_exists = User.query.filter_by(email=email).first()
-    username_exists = User.query.filter_by(username=username).first()
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
 
-    if email_exists:
-        flash('Email is already in use.', category='error')
-    elif username_exists:
-        flash('Username is already in use.', category='error')
-    elif password1 != password2:
-        flash('Passwords do not match!', category='error')
-    elif len(password1) < 8:
-        flash('Password is too short.', category='error')
-    elif len(username) < 2:
-        flash('Username is too short.', category='error')
-    elif len(email) < 4:
-        flash('Email is not valid.', category='error')
-    else:
-        new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user, remember=True)
-        flash('Your account has been created!', category='success')
-        return redirect(url_for('views.home'))
+        if email_exists:
+            flash('Email is already in use.', category='error')
+        elif username_exists:
+            flash('Username is already in use.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match!', category='error')
+        elif len(password1) < 8:
+            flash('Password is too short.', category='error')
+        elif len(username) < 2:
+            flash('Username is too short.', category='error')
+        elif len(email) < 4:
+            flash('Email is not valid.', category='error')
+        else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Your account has been created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
 
