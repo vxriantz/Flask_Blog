c75efca1736ff919145e30de735235eb5d5b7aeb
vxriantz <20067@my.sanctamaria.school.nz>
2026-05-08 09:25:20 +1200

    video 15 - create the create posts route (posts are not posting or saving to database, will fix with next video)

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 
 # import external libraries
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user
+
+# import database
+from . import db
+
+# import from .models user
+from .models import User
 
 #set views blueprint
 views = Blueprint("views", __name__)
@@ -21,4 +27,20 @@ def home():
 # create blog post route function
 # returns create_post.html
 def create_post():
+    if request.method == "POST":
+        title = request.form.get('title')
+        content = request.form.get('content')
+        
+        if not title:
+            flash('Title cannot be empty')
+        elif not content:
+            flash('Blog cannot be empty')
+        else:
+            post = User(title=title, content=content, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category='success')
+            return redirect(url_for('views.home'))
+
+
     return render_template("create_post.html", user=current_user)
