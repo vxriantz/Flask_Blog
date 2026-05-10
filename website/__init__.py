1e644d12e365cbfeae6d5c8a48974f0ddf9a91a4
vxriantz <20067@my.sanctamaria.school.nz>
2026-05-10 12:50:44 +1200

    attempting to fix post database saving issue #1

--- website/__init__.py
+++ website/__init__.py
@@ -25,7 +25,7 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User
+    from .models import User, Post
 
     with app.app_context():
         db.create_all()
--- website/views.py
+++ website/views.py
@@ -1,13 +1,10 @@
-
 # import external libraries
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user
-
 # import database
 from . import db
-
 # import from .models user
-from .models import User
+from .models import Post
 
 #set views blueprint
 views = Blueprint("views", __name__)
@@ -18,29 +15,27 @@ views = Blueprint("views", __name__)
 # home route function
 # returns home.html
 def home():
-    return render_template("home.html", user=current_user)
+    posts = Post.query.all()
+    return render_template("home.html", user=current_user, posts=posts)
 
 
 # create blog post route
 @views.route("/create-post", methods=['GET', 'POST'])
+# user must be logged in to post
 @login_required
-# create blog post route function
-# returns create_post.html
 def create_post():
     if request.method == "POST":
-        title = request.form.get('title')
-        content = request.form.get('content')
-        
+        title = request.form.get("title")
+        content = request.form.get("content")
         if not title:
-            flash('Title cannot be empty')
+            flash('Title cannot be empty', category='error')
         elif not content:
-            flash('Blog cannot be empty')
+            flash('Blog cannot be empty', category='error')
         else:
-            post = User(title=title, content=content, author=current_user.id)
+            post = Post(title=title, content=content, author=current_user.id)
             db.session.add(post)
             db.session.commit()
             flash('Post created!', category='success')
             return redirect(url_for('views.home'))
-
 
     return render_template("create_post.html", user=current_user)
