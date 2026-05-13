e79282f36fa49077e0279347960bffafcb14c502
vxriantz <20067@my.sanctamaria.school.nz>
2026-05-14 10:15:13 +1200

    video 19 - added database for comments, made a relationship between user and comments and post and comments, made a create blog comment route, fixed one line in posts_div.html so comments would drop down to view when "View (number) Comments" is clicked, after creating create blog comment route and db tested to ensure it worked (test=successful), made a delete comment route, cannot delete other users comments (tested and was successful)

--- website/models.py
+++ website/models.py
@@ -12,13 +12,24 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
 
 
 
 # database model for blog post
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), nullable=False)
+    title = db.Column(db.String(150), unique=True)
     content = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+
+# database model for blog comment
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), unique=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 # import database
 from . import db
 # import from .models user
-from .models import User, Post
+from .models import User, Post, Comment
 #set views blueprint
 views = Blueprint("views", __name__)
 
@@ -26,6 +26,7 @@ def home():
 def blog():
     posts = Post.query.all()
     return render_template("blog.html", user=current_user, posts=posts)
+
 
 
 # create blog post route
@@ -65,4 +66,42 @@ def delete_post(id):
         db.session.delete(post)
         db.session.commit()
         flash('Post deleted!', category='success')
+    return redirect(url_for('views.blog'))
+
+
+# blog comment route
+@views.route("/create-comment/<post_id>", methods=['POST'])
+# user must be logged in to create comment
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+    if not text:
+        flash('Comment cannot be empty', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+            flash('Comment added!', category='success')
+        else:
+            flash('Post does not exist', category='error')
+
+    return redirect(url_for('views.blog'))
+
+
+# delete comment route
+@views.route("/delete-comment/<comment_id>")
+# user must be logged in to delete comment
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+        flash('Comment does not exist', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+        flash('Comment deleted!', category='success')
     return redirect(url_for('views.blog'))
